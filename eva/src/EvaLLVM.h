9ac9967dac6394b2cc6b2611c879819f25be15eb
Steeely Dan <steeelydan@posteo.de>
2023-12-07 12:12:50 +0100

    Compile the main function

--- eva/src/EvaLLVM.h
+++ eva/src/EvaLLVM.h
@@ -2,6 +2,7 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Verifier.h"
 
 #ifndef EvaLLVM_h
 #define EvaLLVM_h
@@ -20,7 +21,7 @@ public:
         // auto ast = parser->parser(program);
 
         // 2. Compile to LLVM IR:
-        // compile(ast);
+        compile();
 
         // Print generated code
         module->print(llvm::outs(), nullptr);
@@ -43,6 +44,73 @@ private:
         llvm::raw_fd_ostream outLL(fileName, errorCode);
         module->print(outLL, nullptr);
     }
+
+    void compile()
+    {
+        fn = createFunction("main", llvm::FunctionType::get(builder->getInt32Ty(), false));
+
+        auto result = gen();
+
+        auto i32Result = builder->CreateIntCast(result, builder->getInt32Ty(), true);
+
+        builder->CreateRet(i32Result);
+    }
+
+    llvm::Value *gen()
+    {
+        return builder->getInt32(42);
+    }
+
+    /**
+     * Functions consist of:
+     * - Function type: Param types / return type / varargs?
+     * - Blocks; always there: entry block
+     *     - Optimization takes place here
+     * - Control flow blocks: Branch instructions: Conditionals, jumps
+     */
+    llvm::Function *createFunction(const std::string &fnName, llvm::FunctionType *fnType)
+    {
+        // Function prototype might already be defined
+        auto fn = module->getFunction(fnName);
+
+        if (fn == nullptr)
+        {
+            fn = createFunctionProto(fnName, fnType);
+        }
+
+        createFunctionBlock(fn);
+
+        return fn;
+    }
+
+    llvm::Function *createFunctionProto(const std::string &fnName, llvm::FunctionType *fnType)
+    {
+        auto fn = llvm::Function::Create(fnType, llvm::Function::ExternalLinkage, fnName, *module);
+
+        verifyFunction(*fn);
+
+        return fn;
+    }
+
+    void createFunctionBlock(llvm::Function *fn)
+    {
+        auto entry = createBB("entry", fn);
+        // Emit code exactly into this block
+        builder->SetInsertPoint(entry);
+    }
+
+    /**
+     * If `fn` is passed, block is automatically appended to
+     * the parent function.
+     * Otherwise, block should later be appended manually via
+     * fn->getBasicBlockList().push_back(block)
+     */
+    llvm::BasicBlock *createBB(std::string name, llvm::Function *fn = nullptr)
+    {
+        return llvm::BasicBlock::Create(*ctx, name, fn);
+    }
+
+    llvm::Function* fn;
 
     /**
      * Owns & manages core 'global' data of LLVM's core infrastructure,
